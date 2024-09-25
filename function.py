dcaba2160c283ea341079e5fdd26af4077ff2d66
hell003 <usa497054@gmail.com>
2024-09-24 19:35:19 -0600

    file updated

---
+++ function.py
@@ -0,0 +1,8 @@
+
+def greet():
+    print('hello world')
+
+    #call the function
+    greet()
+
+    print ('outside function')
--- hello.py
+++ hello.py
@@ -3,4 +3,3 @@ message = 'Hello My Name is Sam'
 print('Sam' in message)  
 
 print('sam' not in message)
-
